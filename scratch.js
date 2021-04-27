dd07656b9e91338243ec82df3ec20ce46d2ad653
Eric Vera <6858503+ericvera@users.noreply.github.com>
2021-04-26 18:09:45 -0600

    Remove scratch file.

--- scratch.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const shell = require('shelljs')
-
-const a = shell.pwd()
-
-console.log(a.toString())
-
-console.log(path.resolve('../index.js'))
-console.log(path.join(a.toString(), '../index.js'))
