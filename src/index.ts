1d6f29f86f422e5f9710e8f8196248387ee8aaaf
Eric Vera <6858503+ericvera@users.noreply.github.com>
2022-11-29 17:53:12 -0500

    Optimize builds to only copy files if the project is build

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import { executeCleanFiles } from './helpers/clean.js'
 import { executeCopyFiles } from './helpers/copy.js'
 import { getExecutionEnvironment, shellExecute } from './helpers/shell.js'
 import { getRootTSBConfigPath, getTSBConfigPaths } from './helpers/tsbConfig.js'
-import { getReferencesPaths, getTSConfigPath } from './helpers/tsConfig.js'
+import { getTSConfigPath } from './helpers/tsConfig.js'
 import { enableDebugLogging, log, logList } from './helpers/log.js'
+import ts from 'typescript'
+import path from 'path'
 ;(async () => {
   const executionEnvironment = await getExecutionEnvironment()
 
@@ -38,24 +40,87 @@ import { enableDebugLogging, log, logList } from './helpers/log.js'
   }
 
   let tsbConfigPaths
+  const projects: string[] = []
 
   // 2. Run tsc command with provided args
   if (isBuild) {
-    shellExecute(executionEnvironment, `tsc ${sanitizedArgs.join(' ')}`)
-
     // 2a. Get path for current TSConfig
     const tsConfigPath = await getTSConfigPath(sanitizedArgs)
     log(`TSConfig path: ${tsConfigPath}`)
 
-    // 2b. Get config paths for the command and current references
-    const refPaths = getReferencesPaths(executionEnvironment, tsConfigPath)
-    log(`Found refPaths:`)
-    logList(refPaths)
+    // 2b. Get the list of projects that would be compiled
+    const buildOptions: ts.BuildOptions = { dry: true }
 
-    // 2c. Get tsb configs to copy content
+    const reporter: ts.DiagnosticReporter = (diagnostic) => {
+      if (typeof diagnostic.messageText !== 'string') {
+        throw new Error(
+          `diagnostic.messageText is unexpectedly not a string: '${JSON.stringify(
+            diagnostic.messageText
+          )}'`
+        )
+      }
+
+      const matched = diagnostic.messageText.match(/(?:'[^']*'|^[^']*$)/)
+
+      if (matched?.length !== 1) {
+        throw new Error(
+          `Unexpected format of message that is supposed to contain the project to be built: '${diagnostic.messageText}'`
+        )
+      }
+
+      const projectPath = matched[0].replace(/'/g, '')
+
+      switch (diagnostic.code) {
+        case 6357:
+          projects.push(projectPath)
+          log('Will build:', projectPath)
+          break
+        case 6361:
+          log('Up to date:', projectPath)
+          break
+        default:
+          throw new Error(`Unexpected diagnostic code: '${diagnostic.code}'`)
+      }
+    }
+
+    const buildHost = ts.createSolutionBuilderHost(
+      /*sys*/ undefined,
+      /*createProgram*/ undefined,
+      undefined,
+      reporter
+    )
+
+    const builder = ts.createSolutionBuilder(
+      buildHost,
+      [tsConfigPath],
+      buildOptions
+    )
+
+    // Dry build to get the list of projects that are not up-to-date
+    builder.build()
+
+    // 2c. Run tsc
+    shellExecute(executionEnvironment, `tsc ${sanitizedArgs.join(' ')}`)
+
+    // 2b. Get config paths for the command and current references
+    // const refPaths = getReferencesPaths(executionEnvironment, tsConfigPath)
+
+    const refPaths = projects.map((ref) => path.dirname(ref))
+    if (refPaths.length > 0) {
+      log('Found refPaths to be built:')
+      logList(refPaths)
+    } else {
+      log('Found no refPaths to be build.')
+    }
+
+    // 2d. Get tsb configs to copy content
     tsbConfigPaths = getTSBConfigPaths(refPaths)
-    log('Found tsbConfigPaths:')
-    logList(tsbConfigPaths)
+    if (tsbConfigPaths.length > 0) {
+      log('Found tsbConfigPaths:')
+      logList(tsbConfigPaths)
+    } else {
+      log('No tsbConfigPaths found.')
+    }
   } else {
     tsbConfigPaths = getRootTSBConfigPath()
   }
